b42bbb961bc07b54d77f71555169eac27b75066b
Abhinav Kayastha <111752925+abhinav-kayastha@users.noreply.github.com>
2024-09-08 22:42:38 +0300

    fuck this shit
    i am fed up with git's bullshit

---
+++ ex3/function.c
@@ -0,0 +1,47 @@
+// function.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "function.h"
+
+void generate_random_numbers(int min, int max, int count) {
+    int range = max - min + 1;
+    int *random_numbers = (int *)malloc(count * sizeof(int));
+    if (random_numbers == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+    
+    srand(time(NULL));
+    for (int i = 0; i < count; i++) {
+        int num;
+        bool unique;
+        do {
+            num = rand() % range + min;
+            unique = true;
+            for (int j = 0; j < i; j++) {
+                if (random_numbers[j] == num) {
+                    unique = false;
+                    break;
+                }
+            }
+        } while (!unique);
+        random_numbers[i] = num;
+        printf("%d ", num);
+    }
+    printf("\n");
+    free(random_numbers);
+}
+
+bool validate_inputs(int min, int max, int count) {
+    if (min > max) {
+        printf("Minimum value cannot be greater than maximum value.\n");
+        return false;
+    }
+    if (count > (max - min + 1)) {
+        printf("The number of unique values requested exceeds the range of possible unique values.\n");
+        return false;
+    }
+    return true;
+}
+
