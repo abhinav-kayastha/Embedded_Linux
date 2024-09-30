976ffdff8a2669e156a98a4fac59e6a034f688b3
abhinav-kayastha <Abhinav.Kayastha@metropolia.fi>
2024-09-30 22:35:53 +0300

    finished ex6

---
+++ ex6/build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/abhinav/exercises/ex2/ex6/build/_deps/picotool/"
+};
---
+++ ex6/debug/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/abhinav/exercises/ex2/ex6/debug/_deps/picotool/"
+};
