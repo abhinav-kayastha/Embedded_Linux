b6ea59aa7f8946640daf0d2c717513654882eb69
abhinav-kayastha <Abhinav.Kayastha@metropolia.fi>
2024-10-05 17:24:40 +0300

    ex6 .c file change

--- ex6/src/led.c
+++ ex6/src/led.c
@@ -5,8 +5,13 @@ int main() {
     const uint LED_PIN = PIN_NUM;  // Define the onboard LED pin
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    int counter = 0;
+    int state = 0;
 
     while (true) {
+	counter++;
+	state = counter % 2;
         gpio_put(LED_PIN, 1);
         sleep_ms(500);
         gpio_put(LED_PIN, 0);
